5b1af9c8e067f28c965ad854ad0f199f59a4e786
tbrincatos <88725546+tbrincatos@users.noreply.github.com>
2022-07-27 15:00:43 +0200

    Added Footer component
    - created component with Github link
    - added css

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
+import Footer from "./Footer";
 
 export default function App() {
   return (
     <div className="App">
       <h1>Weather App</h1>
+      <Footer />
     </div>
   );
 }
---
+++ src/Footer.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./Footer.css";
+export default function Footer() {
+  return (
+    <div className="Footer">
+      <footer>
+        <a
+          href="https://github.com/tbrincatos/weather-app-gentilly"
+          target="_blank"
+          title="Go to GitHub account"
+        >
+          Open-source code by Tamsin Brincat👩‍🦰
+        </a>
+      </footer>
+    </div>
+  );
+}
