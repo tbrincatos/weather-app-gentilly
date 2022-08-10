abfec3dc32d71fef034c6c1c4939d779d1ac1110
tbrincatos <88725546+tbrincatos@users.noreply.github.com>
2022-08-10 13:52:21 +0200

    Fixed error

--- src/WeatherCurrentTemperature.js
+++ src/WeatherCurrentTemperature.js
@@ -1,12 +1,10 @@
 import React from "react";
 import "./WeatherCurrentTemperature.css";
 export default function WeatherCurrentTemperature(props) {
-  {
-    return (
-      <div className="WeatherCurrentTemperature">
-        <h2>{Math.round(props.celsiusTemperature)}</h2>
-        <sup>°C</sup>
-      </div>
-    );
-  }
+  return (
+    <div className="WeatherCurrentTemperature">
+      <h2>{Math.round(props.celsiusTemperature)}</h2>
+      <sup>°C</sup>
+    </div>
+  );
 }
