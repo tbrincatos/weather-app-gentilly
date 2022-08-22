e9f1882c4851391401f6ae75526b3ec50ac8ee53
tbrincatos <88725546+tbrincatos@users.noreply.github.com>
2022-08-22 16:27:59 +0200

    Tiny styling improvements
    - made description h4 bigger from 24 to 28px
    - reduced the margin of the app from 40 to 25px

--- src/WeatherCurrent.js
+++ src/WeatherCurrent.js
@@ -9,7 +9,7 @@ export default function WeatherCurrent(props) {
         {" "}
         <h4 className="text-capitalize">{props.report.description}</h4>
       </div>
-      <h5>{props.report.wind}m/s</h5>
+      <h5>{props.report.wind} m/s</h5>
       <div>
         <h2>{Math.round(props.report.temperature)}</h2>
         <sup>°C</sup>
